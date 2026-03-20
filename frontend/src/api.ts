b1b19f5227603c314f3917f1bf464f1fcba63d11
sarawut-hub <sarawut.o@kubota.com>
2026-03-20 11:45:11 +0700

    change frontend to use env

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Extend AxiosRequestConfig to include retry property
 interface RetryConfig extends InternalAxiosRequestConfig {
